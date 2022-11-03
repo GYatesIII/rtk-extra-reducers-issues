1e2db12ceca7e8ade5765b8b910fa4950e82a752
George Yates <me@georgeyatesiii.com>
2022-11-03 10:44:58 -0500

    Initial commit with error

---
+++ src/store.ts
@@ -0,0 +1,50 @@
+import {
+  combineReducers,
+  configureStore,
+  createSlice,
+} from "@reduxjs/toolkit";
+
+interface SliceState {
+    fakeProp: boolean;
+}
+const initialState: SliceState = {
+    fakeProp: false,
+}
+
+const sliceWithExtraReducersAfterSlice = createSlice({
+  name: "sliceWithExtraReducersAfter",
+  initialState,
+  extraReducers: (builder) => {},
+  reducers: {
+    fakeReducer: (state) => {},
+  },
+});
+
+const sliceWithExtraReducersBeforeSlice = createSlice({
+    name: "sliceWithExtraReducersBefore",
+    initialState,
+    reducers: {
+      fakeReducer: (state) => {},
+    },
+    extraReducers: (builder) => {},
+  });
+
+  const sliceWithoutExtraReducersSlice = createSlice({
+    name: "sliceWithoutExtraReducers",
+    initialState,
+    reducers: {
+      fakeReducer: (state) => {},
+    },
+  });
+
+const rootReducer = combineReducers({
+  sliceWithExtraReducersAfter: sliceWithExtraReducersAfterSlice.reducer,
+  sliceWithExtraReducersBefore: sliceWithExtraReducersBeforeSlice.reducer,
+  sliceWithoutExtraReducers: sliceWithoutExtraReducersSlice.reducer,
+});
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+type RootState = ReturnType<typeof rootReducer>;
